dcb58fea2f45950a56b2b124c7c79f298a1e66bd
Saimon <saimon22011@gmail.com>
2024-03-14 11:24:32 +0400

    feat: added a navigation bar for the "What we can offer you" slider.

--- js/script.js
+++ js/script.js
@@ -287,6 +287,7 @@ window.addEventListener('DOMContentLoaded',()=>{
     //Slider
 
     const slides = document.querySelectorAll('.offer__slide'),
+        slider = document.querySelector('.offer__slider'),
         prev = document.querySelector('.offer__slider-prev'),
         next = document.querySelector('.offer__slider-next'),
         total = document.querySelector('#total'),
@@ -316,6 +317,51 @@ window.addEventListener('DOMContentLoaded',()=>{
         slide.style.width = width;
     })
 
+    slider.style.position = 'relative';
+
+    const indicators = document.createElement('ol'),
+        dots = [];
+
+    indicators.classList.add('carousel-indicators');
+    indicators.style.cssText = `
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right: 15%;
+        margin-left: 15%;
+        list-style: none;
+    `;
+    slider.append(indicators);
+
+    for(let i = 0; i < slides.length; i++){
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to',i + 1);
+        dot.style.cssText = `
+            box-sizing: content-box;
+            flex: 0 1 auto;
+            width: 30px;
+            height: 6px;
+            margin-right: 3px;
+            margin-left: 3px;
+            cursor: pointer;
+            background-color: #fff;
+            background-clip: padding-box;
+            border-top: 10px solid transparent;
+            border-bottom: 10px solid transparent;
+            opacity: .5;
+            transition: opacity .6s ease;
+        `;
+        if( i === 0 ){
+            dot.style.opacity = 1;
+        }
+        indicators.append(dot);
+        dots.push(dot);
+    }
+
     next.addEventListener('click',()=>{
         if (offset === +width.slice(0,width.length-2) * (slides.length-1)){
             offset = 0;
@@ -336,6 +382,11 @@ window.addEventListener('DOMContentLoaded',()=>{
         }else{
             current.textContent = slideIndex;
         }
+
+        dots.forEach(dot => {
+            dot.style.opacity = '.5';
+            dots[slideIndex-1].style.opacity = 1;
+        })
     });
 
     prev.addEventListener('click',()=>{
@@ -358,45 +409,33 @@ window.addEventListener('DOMContentLoaded',()=>{
         }else{
             current.textContent = slideIndex;
         }
+
+        dots.forEach(dot => {
+            dot.style.opacity = '.5';
+            dots[slideIndex-1].style.opacity = 1;
+        })
+    });
+
+    dots.forEach(dot => {
+        dot.addEventListener('click',(e)=>{
+            const slideTo = e.target.getAttribute('data-slide-to');
+
+            slideIndex = slideTo;
+            offset = +width.slice(0,width.length-2) * (slideTo-1);
+
+            slidesField.style.transform = `translateX(-${offset}px)`;
+
+            if(slides.length<10){
+                current.textContent = `0${slideIndex}`;
+            }else{
+                current.textContent = slideIndex;
+            }
+
+            dots.forEach(dot => {
+                dot.style.opacity = '.5';
+                dots[slideIndex-1].style.opacity = 1;
+            })
+        })
     })
-    // showSlides(slideIndex);
-    //
-    // if(slides.length<10){
-    //     total.textContent = `0${slides.length}`;
-    // }else{
-    //     total.textContent = slides.length;
-    // }
-    //
-    // function showSlides(n){
-    //     if( n > slides.length){
-    //         slideIndex=1;
-    //     }
-    //
-    //     if(n<1){
-    //         slideIndex = slides.length;
-    //     }
-    //
-    //     slides.forEach(item=> item.style.display = 'none');
-    //
-    //     slides[slideIndex-1].style.display = 'block';
-    //
-    //     if(slides.length<10){
-    //         current.textContent = `0${slideIndex}`;
-    //     }else{
-    //         current.textContent = slideIndex;
-    //     }
-    // }
-    //
-    // function plusSlides(n){
-    //     showSlides(slideIndex+=n);
-    // }
-    //
-    // prev.addEventListener('click',()=>{
-    //     plusSlides(-1);
-    // });
-    //
-    // next.addEventListener('click',()=>{
-    //     plusSlides(1);
-    // })
 
 });
