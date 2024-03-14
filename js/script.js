ae3860ddd7b90deea0362f4ce7dbb5354b267970
Saimon <saimon22011@gmail.com>
2024-03-14 10:03:54 +0400

    chore: added library axios

--- js/script.js
+++ js/script.js
@@ -192,36 +192,19 @@ window.addEventListener('DOMContentLoaded',()=>{
         return await res.json()
     };
 
-    getResource('http://localhost:3000/menu')
+    // getResource('http://localhost:3000/menu')
+    //     .then(data => {
+    //         data.forEach(({img,altimg,title,descr,price} )=>{
+    //             new MenuCard(img,altimg,title,descr,price, '.menu .container').render();
+    //         });
+    //     });
+
+    axios.get('http://localhost:3000/menu')
         .then(data => {
-            data.forEach(({img,altimg,title,descr,price} )=>{
+            data.data.forEach(({img,altimg,title,descr,price} )=>{
                 new MenuCard(img,altimg,title,descr,price, '.menu .container').render();
             });
-        });
-    // getResource('http://localhost:3000/menu')
-    //     .then(data => createCard(data));
-    //
-    // function createCard(data){
-    //     data.forEach(({img,altimg,title,descr,price}) => {
-    //         const element = document.createElement('div');
-    //
-    //         element.classList.add('menu__item');
-    //
-    //         element.innerHTML = `
-    //             <img src=${img} alt=${altimg}>
-    //             <h3 class="menu__item-subtitle">${title}</h3>
-    //             <div class="menu__item-descr">${descr}</div>
-    //             <div class="menu__item-divider"></div>
-    //             <div class="menu__item-price">
-    //                 <div class="menu__item-cost">Цена:</div>
-    //                 <div class="menu__item-total"><span>${price}</span> грн/день</div>
-    //             </div>
-    //         `;
-    //
-    //         document.querySelector('.menu .container').append(element);
-    //     })
-    // }
-
+        })
 
     //FORMS
     const forms = document.querySelectorAll('form');
@@ -300,9 +283,5 @@ window.addEventListener('DOMContentLoaded',()=>{
             closeModal();
         }, 4000)
     }
-
-    fetch('http://localhost:3000/menu')
-        .then(data=>data.json())
-        .then(res => console.log(res))
 
 });
