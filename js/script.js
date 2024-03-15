d340aa4769f4bae0c9d49d05e5aa395a036b54e0
Saimon <saimon22011@gmail.com>
2024-03-15 10:27:18 +0400

    refactor: removed duplicates in the next and prev EventListener

--- js/script.js
+++ js/script.js
@@ -365,6 +365,18 @@ window.addEventListener('DOMContentLoaded',()=>{
     function deleteNotDigits(str){
         return +str.replace(/\D/g, ' ')
     }
+    function updateSliderStatus() {
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+
+        dots.forEach(dot => {
+            dot.style.opacity = '.5';
+            dots[slideIndex - 1].style.opacity = 1;
+        });
+    }
 
     next.addEventListener('click',()=>{
         if (offset === deleteNotDigits(width) * (slides.length-1)){
@@ -381,16 +393,7 @@ window.addEventListener('DOMContentLoaded',()=>{
             slideIndex++;
         }
 
-        if(slides.length<10){
-            current.textContent = `0${slideIndex}`;
-        }else{
-            current.textContent = slideIndex;
-        }
-
-        dots.forEach(dot => {
-            dot.style.opacity = '.5';
-            dots[slideIndex-1].style.opacity = 1;
-        })
+        updateSliderStatus()
     });
 
     prev.addEventListener('click',()=>{
@@ -408,16 +411,7 @@ window.addEventListener('DOMContentLoaded',()=>{
             slideIndex--;
         }
 
-        if(slides.length<10){
-            current.textContent = `0${slideIndex}`;
-        }else{
-            current.textContent = slideIndex;
-        }
-
-        dots.forEach(dot => {
-            dot.style.opacity = '.5';
-            dots[slideIndex-1].style.opacity = 1;
-        })
+        updateSliderStatus()
     });
 
     dots.forEach(dot => {
@@ -425,20 +419,11 @@ window.addEventListener('DOMContentLoaded',()=>{
             const slideTo = e.target.getAttribute('data-slide-to');
 
             slideIndex = slideTo;
-            offset = +width.replace(/\D/g, ' ') * (slideTo-1);
+            offset = deleteNotDigits(width) * (slideTo-1);
 
             slidesField.style.transform = `translateX(-${offset}px)`;
 
-            if(slides.length<10){
-                current.textContent = `0${slideIndex}`;
-            }else{
-                current.textContent = slideIndex;
-            }
-
-            dots.forEach(dot => {
-                dot.style.opacity = '.5';
-                dots[slideIndex-1].style.opacity = 1;
-            })
+            updateSliderStatus()
         })
     })
 
